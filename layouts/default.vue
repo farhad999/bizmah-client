75217945778b6d014bc4a353e2172ebd87aa282b
Farhad  Shikder <frd1952@gmail.com>
2023-11-22 20:40:21 +0600

    Simple Top bar added

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+    <!-- Top Bar -->
+    <div class="top-bar">
+      <p>
+        FREE SHIPPING FOR ORDERS ON AND ABOVE BDT2500 WITHIN Bangladesh
+      </p>
+    </div>
+    <Nuxt/>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    Footer,
+  }
+}
+</script>
