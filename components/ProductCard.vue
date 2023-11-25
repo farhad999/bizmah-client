5785d1406364f94d826cefbc78c31cc7d08d088b
Farhad  Shikder <frd1952@gmail.com>
2023-11-25 18:30:28 +0600

    Update ProductCard.vue

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,31 +1,36 @@
 <template>
-  <div class="product-card">
-    <!-- Product Media -->
-    <div class="product-media">
-      <div>
-        <img :src="`https://picsum.photos/300/400?random=${product}`"/>
+  <nuxt-link :to="`/product/${product.slug}`">
+    <div class="product-card">
+      <!-- Product Media -->
+      <div class="product-media">
+        <div>
+          <img :src="computeImageUrl(product.image)" alt="product-image"/>
+        </div>
+
+        <div class="badge discount">60% Off</div>
+        <div class="quick-view-btn">
+          <button>Quick View</button>
+        </div>
+
       </div>
 
-      <div class="badge discount">60% Off</div>
-      <div class="quick-view-btn">
-        <button>Quick View</button>
+      <div class="product-card-bottom">
+        <div class="product-title">{{ product.name }}</div>
+        <div class="price-box">
+          <span class="old-price">{{ product.min_old_price }}</span>
+          <span class="new-price">{{ product.min_price }}</span>
+        </div>
       </div>
 
     </div>
-
-    <div class="product-card-bottom">
-      <div class="product-title">Product 1</div>
-      <div class="price-box">
-        <span class="old-price">$120.00</span>
-        <span class="new-price">$100.00</span>
-      </div>
-    </div>
-
-  </div>
+  </nuxt-link>
 </template>
 
 <script>
+import {computeImageUrl} from "../utils/common";
+
 export default {
+  methods: {computeImageUrl},
   props: {
     product: {
       type: Object | Number,
