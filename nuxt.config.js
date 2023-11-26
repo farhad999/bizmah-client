dfed42fe48130dbb40ae10547389da3da213ebef
Farhad  Shikder <frd1952@gmail.com>
2023-11-27 02:08:23 +0600

    filters added

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/index.js'
+    {src: '~/plugins/index.js', ssr: false},
+    {src: '~/plugins/filters.js', ssr: true},
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ plugins/filters.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+
+Vue.filter('priceFormat', function (value, size = 0) {
+  if (value === undefined) return '';
+
+  let number = parseFloat(value);
+
+  return 'BDT ' + number.toLocaleString();
+})
+
+Vue.filter('capitalize', function (value) {
+  if (!value) return '';
+  value = value.toString();
+  return value.charAt(0).toUpperCase() + value.slice(1);
+})
