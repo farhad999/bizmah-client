9dee11e0f37b90cd46a274c53d6482ce5c80e460
Farhad  Shikder <frd1952@gmail.com>
2023-12-03 23:32:48 +0600

    Error Layout Added

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card my-3">
+    <div class="card-body">
+      <div class="d-flex flex-column align-items-center">
+        <h3>{{ error.statusCode }}</h3>
+        <div>{{ error.message }}</div>
+        <nuxt-link to="/" class="btn btn-primary btn-sm mt-2">Home</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+};
+</script>
